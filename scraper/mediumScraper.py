f69e12c37424c4783738dcc10a209836b9eaa7c6
Ansh Godha <ag759@cornell.edu>
2020-07-13 18:20:53 +0530

    Selenium integration allowing scrolling and getting more articles

--- scraper/mediumScraper.py
+++ scraper/mediumScraper.py
@@ -1,5 +1,7 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
 
 BASE_URL = "https://medium.com/topic/"
 
@@ -20,10 +22,11 @@ class Article:
     A general class to represent an article, paves way for better abstraction
     """
 
-    def __init__(self, title, blurb, link, author, datePublished):
+    def __init__(self, title: str, blurb: str, link: str, author: str, datePublished: str):
+        qmark_index = link.find('?')
         self.title = title
         self.blurb = blurb
-        self.link = link
+        self.link = 'https://medium.com/' + link[:qmark_index]
         self.author = author
         self.datePublished = datePublished
 
@@ -35,11 +38,36 @@ class Article:
         return self.__str__()
 
 
-def findArticles(url, keyword=None):
+def fetchWebPageSourceAfterScroll(url, numScrolls=9):
+
+    # Configure Chrome options
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    # So we don't need to actually open a new Window
+    options.add_argument('--headless')
+
+    # Instantiate Chromium driver
+    driver = webdriver.Chrome(
+        "./chromedriver", chrome_options=options)
+
+    # Load the URL
+    driver.get(url)
+    count = 0
+    while count <= numScrolls:
+        # Get the last such instance of this URL
+        allSections = driver.find_elements_by_tag_name('section')
+        lastElement = allSections[-1]
+        driver.execute_script("arguments[0].scrollIntoView();", lastElement)
+        time.sleep(0.5)
+        count += 1
+
+    return driver.page_source
+
+
+def findArticles(page_source, keyword=None):
 
     # Initialize BeautifulSoup object
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.text, 'html.parser')
+    soup = BeautifulSoup(page_source, 'lxml')
 
     # Get all section tags with the given class
     articleContainers: bs4.element.ResultSet = soup.findAll(
@@ -48,10 +76,8 @@ def findArticles(url, keyword=None):
     articles = []
 
     for container in articleContainers:
-
         # Get title link
-        link = str(container.find(
-            class_="ap q eb cc ec cd gd hp hq as av ge eh ei au").a['href'])
+        link = str(container.find('a')['href'])
 
         # Get blurb of article
         blurb = str(container.find(
@@ -73,3 +99,13 @@ def findArticles(url, keyword=None):
         articles.append(Article(title, blurb, link, author, date))
 
     return articles
+
+
+def scrape(url):
+    ps = fetchWebPageSourceAfterScroll(url)
+    articles = findArticles(ps)
+    print(len(articles))
+
+
+if __name__ == "__main__":
+    scrape(MEDIUM_URL)
