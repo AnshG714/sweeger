e8e26ab9afe5196a31acf7a499bac0d49a5f6c52
Ansh Godha <ag759@cornell.edu>
2020-07-14 12:59:54 +0530

    Resolve VSCode unresolved import issue, refactor current Article class to subclass the common ContentItem class

--- scraper/mediumScraper.py
+++ scraper/mediumScraper.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
+from common import ContentItem
 
 BASE_URL = "https://medium.com/topic/"
 
@@ -17,25 +18,13 @@ TOPICS = ["technology", "software-engineering",
 MEDIUM_URL = BASE_URL + "programming"
 
 
-class Article:
+class Article(ContentItem):
     """
     A general class to represent an article, paves way for better abstraction
     """
 
     def __init__(self, title: str, blurb: str, link: str, author: str, datePublished: str):
-        qmark_index = link.find('?')
-        self.title = title
-        self.blurb = blurb
-        self.link = 'https://medium.com/' + link[:qmark_index]
-        self.author = author
-        self.datePublished = datePublished
-
-    def __str__(self):
-        # f-string formatting will only work on Python 3.6+
-        return f'{{Title: {self.title}, Blurb: {self.blurb}, Link: {self.link}, Author: {self.author}}}, Date Published: {self.datePublished}}}'
-
-    def __repr__(self):
-        return self.__str__()
+        super().__init__(title, blurb, link, author, datePublished)
 
 
 def fetchWebPageSourceAfterScroll(url, numScrolls=9):
