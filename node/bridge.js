59b25e9b47406500d3055aa865ed999b7bc0d875
Ansh Godha <ag759@cornell.edu>
2020-07-15 23:48:21 +0530

    How to parse?

--- node/bridge.js
+++ node/bridge.js
@@ -1,32 +1,40 @@
 const { PythonShell } = require("python-shell");
 
-const getScraperResults = (keywords) => {
-  return new Promise((resolve, reject) => {
-    console.log("Initializing JS Bridge...");
+const getScraperResults = (keywords, callback) => {
+  console.log("Initializing JS Bridge...");
 
-    const script = new PythonShell("./../scraper/js_bridge.py", {
-      args: [`[${String(keywords)}]`],
-    });
-
-    script.on("message", function (message) {
-      // received a message sent from the Python script (a simple "print" statement)
-      try {
-        // Uncomment the following line to see live output.
-        // console.log(message.toString());
-        resolve(JSON.parse(message.toString()));
-      } catch (_) {}
-      // console.log(message);
-    });
-
-    script.end(function (err, code, signal) {
-      if (err) reject(err);
-      console.log("The exit code was: " + code);
-      console.log("The exit signal was: " + signal);
-      console.log("finished");
-    });
+  const script = new PythonShell("./../scraper/js_bridge.py", {
+    args: [`[${String(keywords)}]`],
   });
+
+  let json;
+
+  script.on("message", function (message) {
+    // received a message sent from the Python script (a simple "print" statement)
+    try {
+      // Uncomment the following line to see live output.
+      console.log(message.toString());
+      json = JSON.parse(message.toString());
+      callback(json);
+    } catch (_) {
+      json = {};
+    }
+    // console.log(message);
+  });
+
+  // script.end(function (err, code, signal) {
+  //   if (err) throw err;
+  //   console.log("The exit code was: " + code);
+  //   console.log("The exit signal was: " + signal);
+  //   console.log("finished");
+  // });
+
+  // return json;
 };
 
-getScraperResults(["Angular", "TensorFlow"]);
+getScraperResults(["Angular", "TensorFlow"], function (response) {
+  console.log("-------- THIS IS THE FINAL JSON ----------");
+  console.log(response);
+});
 
 module.exports = getScraperResults;
