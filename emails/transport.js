dbb744b7dd6bd8f14adcf430436a1e3527cd2833
Ansh Godha <ag759@cornell.edu>
2020-07-16 14:48:48 +0530

    Add html generator

--- emails/transport.js
+++ emails/transport.js
@@ -1,7 +1,7 @@
 "use strict";
 const nodemailer = require("nodemailer");
-const nodecron = require("node-cron")
-
+const nodecron = require("node-cron");
+const getScraperResults = require("../node/bridge");
 
 let testAccount = nodemailer.createTestAccount();
 
@@ -15,30 +15,37 @@ nodecron.schedule("* * * * Monday", function () {
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: 'lladke7yye36ycji@ethereal.email', // generated ethereal user
-      pass: 'zMWPYCWyS8G48bVmWT', // generated ethereal password
-    }
+      user: "lladke7yye36ycji@ethereal.email", // generated ethereal user
+      pass: "zMWPYCWyS8G48bVmWT", // generated ethereal password
+    },
   });
-  
+
   let transportOptions = {
     from: '"MLH Fellowship" <foo@example.com>', // sender address
     to: "bar@example.com, baz@example.com", // list of receivers
-    subject: "MLH - Your weekly suggestons for skillbuilding!", // Subject line
+    subject: "MLH - Your weekly suggestions for skillbuilding!", // Subject line
     text: "Your weekly email.", // plain text body
-    html: "<b>Your suggestions here!</b>"
+    html: "<b>Your suggestions here!</b>",
   };
-  
+
   transporter.sendMail(transportOptions, function (err, info) {
-    
     if (err) {
       console.log(err);
     } else {
       console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
     }
   });
-  
 });
 
+async function generateHTML(keywords) {
+  let results = await getScraperResults(keywords);
+  let html = "";
+  results.forEach((obj) => {
+    html += `<h4><a href="${obj.link}">${obj.title}</a></h4>`;
+    html += `<h5>By ${obj.author}</h5><br />`;
+  });
+  console.log(html);
+  return html;
+}
 
-
-
+generateHTML(["Keras"]);
