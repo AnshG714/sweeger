fc103a677446f434d39671b1bba2c1e4fec9f591
Janie <janiewang26@gmail.com>
2020-07-17 10:26:37 -0700

    Fix VerifyEmail page database issue

--- react-firebase/client/src/pages/VerifyEmail.js
+++ react-firebase/client/src/pages/VerifyEmail.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { withRouter, Redirect } from "react-router";
 import { Alert, Button } from 'react-bootstrap';
+import app from "../config/Firebase.js";
 import { AuthContext } from "../actions/Auth.js";
 
 export default function VerifyEmail() {
@@ -8,7 +9,19 @@ export default function VerifyEmail() {
 
     const { currentUser } = useContext(AuthContext);
 
+    const onCreate= () => {
+        const db = app.database();
+        db.ref("users/" + currentUser.uid).set({
+            email: currentUser.email,
+        });
+        db.ref("prefs/" + currentUser.uid).set({
+            frequency: "null",
+            keywords: "null",
+        });
+    }
+
     if (currentUser && currentUser.emailVerified) {
+        onCreate();
         return <Redirect to="/" />;
     }
 
