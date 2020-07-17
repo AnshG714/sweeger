1f46674791223b14ae46cfe3d36a62e440eee042
Darren Schachter <dts86@cornell.edu>
2020-07-17 14:34:47 -0400

    Fix scraper error

--- scraper/webScraper.py
+++ scraper/webScraper.py
@@ -64,41 +64,44 @@ def findArticles(page_source, keywords):
     articles = []
 
     for container in articleContainers:
-        # print(container)
-        # Get title link
-        link = str(container.find('a')['href'])
-        if link[0] == '/':
-            link = "https://www.medium.com" + link
-
-        # Get blurb of article
-        blurb = str(container.find(
-            class_="cc gb gc gd av ge eh as ei au ch").a.string)
-
-        # Find the name of the Author
-        authorDetailContainer = container.find(
-            class_="cc b cd ce cf cg as av ek eh ei au ap q")
-        author = str(authorDetailContainer.find('a').string)
-
-        # Get date of publishing
-        date = str(container.find(class_="gp n cn").find(
-            text=True, recursive=False))
-
-        # Get title of article
-
-        titleContainer = container.find(
-            class_="ap q eb cc ec cd gd hp hq as av ge eh ei au") or container.find(class_="ap q fs bv ft bw hd io ip as av he ch fy au")
         try:
-            title = str(titleContainer.a.string)
-        except:
-            print("error occured in", str(soup.find('title').string))
+            # print(container)
+            # Get title link
+            link = str(container.find('a')['href'])
+            if link[0] == '/':
+                link = "https://www.medium.com" + link
 
-        if keywords:
-            for keyword in keywords:
-                if keyword in blurb or keyword in title:
-                    articles.append(Article(title, blurb, link, author, date))
-                    break
-        else:
-            articles.append(Article(title, blurb, link, author, date))
+            # Get blurb of article
+            blurb = str(container.find(
+                class_="cc gb gc gd av ge eh as ei au ch").a.string)
+
+            # Find the name of the Author
+            authorDetailContainer = container.find(
+                class_="cc b cd ce cf cg as av ek eh ei au ap q")
+            author = str(authorDetailContainer.find('a').string)
+
+            # Get date of publishing
+            date = str(container.find(class_="gp n cn").find(
+                text=True, recursive=False))
+
+            # Get title of article
+
+            titleContainer = container.find(
+                class_="ap q eb cc ec cd gd hp hq as av ge eh ei au") or container.find(class_="ap q fs bv ft bw hd io ip as av he ch fy au")
+            try:
+                title = str(titleContainer.a.string)
+            except:
+                print("error occured in", str(soup.find('title').string))
+
+            if keywords:
+                for keyword in keywords:
+                    if keyword in blurb or keyword in title:
+                        articles.append(Article(title, blurb, link, author, date))
+                        break
+            else:
+                articles.append(Article(title, blurb, link, author, date))
+        except:
+            continue
 
     return articles
 
